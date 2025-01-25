f2ed5acb6684b624b81b8ddb1ebfc4de5de0eaf0
Dzhake <andryzindmi@gmail.com>
2025-01-25 21:48:11 +0300

    Finish selection

---
+++ MonoPlus/MonoPlus.Drawing/Graphics.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoPlus.Graphics;
+
+public static class Graphics
+{
+    public static GraphicsDeviceManager? deviceManager;
+    public static SpriteBatch? spriteBatch;
+    public static GraphicsDevice? device;
+    public static Texture2D? Pixel;
+
+    public static void OnGameCreated(Game game)
+    {
+        deviceManager = new GraphicsDeviceManager(game);
+    }
+
+    public static void Initialize(Game game)
+    {
+        if (deviceManager == null) throw new InvalidOperationException("graphicsManager is null!");
+        device = game.GraphicsDevice;
+        spriteBatch = new SpriteBatch(game.GraphicsDevice);
+        Pixel = new(device, 1, 1);
+        Pixel.SetData([Color.White]);
+    }
+
+    #region BaseFunctions
+    public static void Clear(Color color) => device?.Clear(color);
+
+    public static void DrawTexture(Texture2D texture, Vector2 position, Color color) => spriteBatch?.Draw(texture, position, color);
+
+    public static void DrawTexture(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color,
+    float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects = SpriteEffects.None, float layerDepth = 0) => spriteBatch?.Draw(texture,
+    position, sourceRectangle, color, rotation, origin, scale, effects, layerDepth);
+
+    public static void DrawText(SpriteFont font, string text, Vector2 position, Color color) => spriteBatch?.DrawString(font, text, position, color);
+
+    public static void DrawText(SpriteFont font, string text, Vector2 position, Color color, float rotation, Vector2? origin = null, Vector2? scale = null, SpriteEffects effects = SpriteEffects.None, float layerDepth = 0, bool rtl = false) => spriteBatch?.DrawString(font, text, position, color, rotation, origin ?? Vector2.One, scale ?? Vector2.One, effects, layerDepth, rtl);
+
+    public static void Begin(SpriteSortMode sortMode = SpriteSortMode.Deferred, BlendState? blendState = null, SamplerState? samplerState = null, DepthStencilState? depthStencilState = null, RasterizerState? rasterizerState = null, Effect? effect = null, Matrix? transformMatrix = null) => spriteBatch?.Begin(sortMode, blendState, samplerState, depthStencilState, rasterizerState, effect, transformMatrix);
+
+    public static void End() => spriteBatch?.End();
+    #endregion
+
+    #region AdvancedFunctions
+    public static void DrawLine(Vector2 p1, Vector2 p2, Color color, float width = 1f)
+    {
+        if (Pixel is null) throw new InvalidOperationException("DrawLine was called, but Pixel is null!");
+        DrawTexture(Pixel, p1, new Rectangle?(), color, (float)Math.Atan2(p2.Y - p1.Y, p2.X - p1.X), new Vector2(0f, 0.5f), new Vector2((p1 - p2).Length(), width));
+    }
+
+    public static void DrawRect(Vector2 p1, Vector2 p2, Color col, bool filled = true, float borderWidth = 1f)
+    {
+        if (Pixel is null) throw new InvalidOperationException("DrawRect was called, but Pixel is null!");
+        if (filled)
+            DrawTexture(Pixel, p1, new Rectangle?(), col, 0f, Vector2.Zero, new Vector2(-(p1.X - p2.X), -(p1.Y - p2.Y)));
+        else
+        {
+            float num = borderWidth / 2f;
+            DrawLine(new Vector2(p1.X, p1.Y + num), new Vector2(p2.X, p1.Y + num), col, borderWidth);
+            DrawLine(new Vector2(p1.X + num, p1.Y + borderWidth), new Vector2(p1.X + num, p2.Y - borderWidth), col, borderWidth);
+            DrawLine(new Vector2(p2.X, p2.Y - num), new Vector2(p1.X, p2.Y - num), col, borderWidth);
+            DrawLine(new Vector2(p2.X - num, p2.Y - borderWidth), new Vector2(p2.X - num, p1.Y + borderWidth), col, borderWidth);
+        }
+    }
+    #endregion
+}
--- MonoPlus/MonoPlus.Input/TextField.cs
+++ MonoPlus/MonoPlus.Input/TextField.cs
@@ -11,39 +11,138 @@ public class TextField
     public int CursorPos;
     public int SelectionStart;
     public int SelectionEnd;
+    public int SelectionLength => SelectionEnd - SelectionStart;
+
 
     public void Update()
     {
-        if (Input.FocusedTextField != this) return; 
-        
-        text.Append(Input.KeyString);
-        if (Input.Pressed(Keys.Back) && CursorPos > 0)
-            text.Remove(CursorPos - 1, 1);
-        if (Input.Pressed(Keys.Delete) && CursorPos < text.Length)
-            text.Remove(CursorPos, 1);
-        if (Input.Pressed(Keys.Left))
-        {
-            CursorPos = Math.Clamp(CursorPos - 1, 0, text.Length);
-            if (!Input.Shift)
-                ResetSelection();
-        }
-
-        if (Input.Pressed(Keys.Right))
-        {
-            CursorPos = Math.Clamp(CursorPos + 1, 0, text.Length);
-            if (!Input.Shift)
-                ResetSelection();
-        }
-
-        if (Input.Pressed(Keys.End))
-            CursorPos = text.Length;
-        if (Input.Pressed(Keys.Home))
-            CursorPos = 0;
+        if (Input.FocusedTextField != this || HandleInput() || Input.KeyString is null) return;
+        text.Insert(CursorPos, Input.KeyString);
+        CursorPos += Input.KeyString.Length;
     }
 
-    private void ResetSelection()
+    protected bool HandleInput()
+    {
+        if (Input.Pressed(Keys.Back))
+        {
+            if (!TryRemoveSelection() && CursorPos > 0) RemoveSymbol(CursorPos - 1, true);
+        }
+        else if (Input.Pressed(Keys.Delete))
+        {
+            if (!TryRemoveSelection() && CursorPos < text.Length) RemoveSymbol(CursorPos);
+        }
+
+        else if (Input.Pressed(Keys.Left))
+            MoveCursor(CursorPos - 1);
+        else if (Input.Pressed(Keys.Right))
+            MoveCursor(CursorPos + 1);
+
+        else if (Input.Pressed(Keys.End))
+            MoveCursor(text.Length);
+        else if (Input.Pressed(Keys.Home))
+            MoveCursor(0);
+        /*else if (Input.Ctrl)
+        {
+            bool cut = Input.Pressed(Keys.X);
+            if ((Input.Pressed(Keys.C) || cut) && SelectionLength > 0)
+            {
+                SDL.SDL_SetClipboardText(GetSelection());
+                if (cut) RemoveSelection();
+            }
+            else if (Input.Pressed(Keys.V))
+            {
+                TryRemoveSelection();
+                text.Insert(CursorPos, SDL.SDL_GetClipboardText());
+            }
+            return true;
+        }*/
+        else
+            return false;
+        return true;
+    }
+
+
+    public bool TryRemoveSelection()
+    {
+        if (SelectionStart == SelectionEnd) return false;
+        RemoveSelection();
+        return true;
+    }
+
+    public void RemoveSelection()
+    {
+        if (CursorPos == SelectionEnd) CursorPos = SelectionStart;
+        text.Remove(SelectionStart, SelectionLength);
+        ResetSelection();
+    }
+
+    public string GetSelection()
+    {
+        return text.ToString().Substring(SelectionStart, SelectionLength);
+    }
+
+
+    protected void RemoveSymbol(int index, bool moveCursor = false)
+    {
+        text.Remove(index, 1);
+        if (moveCursor) CursorPos = Math.Clamp(CursorPos - 1, 0, text.Length);
+    }
+
+    /// <summary>
+    /// Moves bound of selection which is at same position as <see cref="CursorPos"/>. Call BEFORE changing <see cref="CursorPos"/>!
+    /// </summary>
+    /// <param name="amount">Amount to move</param>
+    protected void MoveSelection(int amount)
+    {
+        if (SelectionStart == SelectionEnd)
+        {
+            switch (amount)
+            {
+                case 0:
+                    return;
+                case < 0:
+                    SelectionEnd = CursorPos;
+                    SelectionStart = CursorPos + amount;
+                    break;
+                case > 0:
+                    SelectionStart = CursorPos;
+                    SelectionEnd = CursorPos + amount;
+                    break;
+            }
+            SelectionStart = Math.Clamp(SelectionStart, 0, text.Length);
+            SelectionEnd = Math.Clamp(SelectionEnd, 0, text.Length);
+            return;
+        }
+
+        if (CursorPos == SelectionStart)
+            SelectionStart = Math.Clamp(SelectionStart + amount, 0, text.Length);
+        else if (CursorPos == SelectionEnd)
+            SelectionEnd = Math.Clamp(SelectionEnd + amount, 0, text.Length);
+        else
+            ResetSelection();
+
+        if (SelectionStart == SelectionEnd) ResetSelection();
+
+        if (SelectionEnd < SelectionStart)
+            (SelectionEnd, SelectionStart) = (SelectionStart, SelectionEnd);
+    }
+
+    protected void ResetSelection()
     {
         SelectionStart = 0;
         SelectionEnd = 0;
+    }
+
+    protected void MoveCursor(int to)
+    {
+        if (Input.Shift)
+            MoveSelection(to - CursorPos);
+        else if (SelectionLength > 0)
+        {
+            ResetSelection();
+            return;
+        }
+            
+        CursorPos = Math.Clamp(to, 0, text.Length);
     }
 }
